810e213a1d0aca65da2e887382d20568bfd3bc85
Aleksbgbg <kozichka01@abv.bg>
2018-08-20 21:12:33 +0100

    [Server] Create WebSocketServer in Main, add StreamChat service, and log the serveri nformation to the console.

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,9 +1,24 @@
 ﻿namespace Twitch.Streams.Server
 {
+    using System;
+
+    using Twitch.Streams.Server.Channels;
+
+    using WebSocketSharp.Server;
+
     internal static class Program
     {
         private static void Main()
         {
+            WebSocketServer webSocketServer = new WebSocketServer(Constants.Port);
+            webSocketServer.AddWebSocketService<StreamChat>("/StreamChat");
+
+            webSocketServer.Start();
+
+            Console.WriteLine("Established server at {0} on port {1}.", webSocketServer.Address, webSocketServer.Port);
+            Console.ReadKey();
+
+            webSocketServer.Stop();
         }
     }
 }
