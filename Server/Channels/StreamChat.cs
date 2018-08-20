68777abda7f3148dd224027f8d1c0bb970c982e9
Aleksbgbg <kozichka01@abv.bg>
2018-08-20 21:11:10 +0100

    [Server] Add StreamChat class, which informs users on opening and closing of the connection and message received, and logs this to the console.

---
+++ Server/Channels/StreamChat.cs
@@ -0,0 +1,28 @@
+﻿namespace Twitch.Streams.Server.Channels
+{
+    using System;
+
+    using WebSocketSharp;
+    using WebSocketSharp.Server;
+
+    internal class StreamChat : WebSocketBehavior
+    {
+        protected override void OnOpen()
+        {
+            Sessions.Broadcast("User joined.");
+            Console.WriteLine("User joined.");
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Sessions.Broadcast(e.Data);
+            Console.WriteLine("User says: {0}", e.Data);
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Sessions.Broadcast("User left.");
+            Console.WriteLine("User left.");
+        }
+    }
+}
