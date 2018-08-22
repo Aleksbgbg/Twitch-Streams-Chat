75a0d916aa7a44b0b0e2c40e7a2a7c496d2888bb
Aleksbgbg <kozichka01@abv.bg>
2018-08-22 11:08:01 +0100

    [RegisterStream] Add socket behaviour which accepts stream registrations.

---
+++ Server/Channels/RegisterStream.cs
@@ -0,0 +1,19 @@
+﻿namespace Twitch.Streams.Server.Channels
+{
+    using System;
+
+    using Twitch.Streams.Server.EventArgs;
+
+    using WebSocketSharp;
+    using WebSocketSharp.Server;
+
+    internal class RegisterStream : WebSocketBehavior
+    {
+        internal event EventHandler<StreamRegisteredEventArgs> StreamRegistered;
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            StreamRegistered?.Invoke(this, new StreamRegisteredEventArgs(e.Data));
+        }
+    }
+}
