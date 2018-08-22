20a1d9139f0084862c4becdfec8fdd990c3f24fc
Aleksbgbg <kozichka01@abv.bg>
2018-08-22 09:54:03 +0100

    [Message] Add class to store information about sent messages.

---
+++ Server/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿namespace Twitch.Streams.Server.Models
+{
+    using Newtonsoft.Json;
+
+    internal class Message
+    {
+        [JsonConstructor]
+        public Message(string sender, string content)
+        {
+            Sender = sender;
+            Content = content;
+        }
+
+        [JsonProperty("Sender")]
+        public string Sender { get; }
+
+        [JsonProperty("Content")]
+        public string Content { get; }
+    }
+}
