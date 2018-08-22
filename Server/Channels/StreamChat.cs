3afa16fc52f49a756510f0ca3a50dcced35b41d4
Aleksbgbg <kozichka01@abv.bg>
2018-08-22 11:39:19 +0100

    [StreamChat] Add ServiceClosed event, which allows unregistering a StreamChat when all users on the service disconnect.

--- Server/Channels/StreamChat.cs
+++ Server/Channels/StreamChat.cs
@@ -11,12 +11,29 @@
 
     internal class StreamChat : WebSocketBehavior
     {
+        internal event EventHandler ServiceClosed;
+
         protected override void OnMessage(MessageEventArgs e)
         {
             Sessions.Broadcast(e.Data);
 
             Message receivedMessage = JsonConvert.DeserializeObject<Message>(e.Data);
             Console.WriteLine("{0}: {1}", receivedMessage.Sender, receivedMessage.Content);
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            OnServiceClosed();
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            OnServiceClosed();
+        }
+
+        private void OnServiceClosed()
+        {
+            ServiceClosed?.Invoke(this, EventArgs.Empty);
         }
     }
 }
