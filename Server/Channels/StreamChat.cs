a5515baef8c6863fd28993afcba09fa17caf35cb
Aleksbgbg <kozichka01@abv.bg>
2018-08-22 10:19:18 +0100

    [StreamChat] Deserialise received messages, and log them to the console.

--- Server/Channels/StreamChat.cs
+++ Server/Channels/StreamChat.cs
@@ -1,5 +1,11 @@
 ﻿namespace Twitch.Streams.Server.Channels
 {
+    using System;
+
+    using Newtonsoft.Json;
+
+    using Twitch.Streams.Server.Models;
+
     using WebSocketSharp;
     using WebSocketSharp.Server;
 
@@ -8,6 +14,9 @@
         protected override void OnMessage(MessageEventArgs e)
         {
             Sessions.Broadcast(e.Data);
+
+            Message receivedMessage = JsonConvert.DeserializeObject<Message>(e.Data);
+            Console.WriteLine("{0}: {1}", receivedMessage.Sender, receivedMessage.Content);
         }
     }
 }
