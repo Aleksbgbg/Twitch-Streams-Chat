f7511cc3bb8f3171656ab8d0630de941362d3282
Aleksbgbg <kozichka01@abv.bg>
2018-08-22 10:22:18 +0100

    [Client] Create class which encapsulates a WebSocket, allowing sending of messages, and also forwarding OnMessage events to a handler.

--- Client/main.js
+++ Client/main.js
@@ -1,5 +1,18 @@
-const serverSocket = new WebSocket("ws://192.168.0.15:8001/StreamChat");
+class UserConnection {
+    constructor(name, onMessage) {
+        this.name = name;
+        this.serverSocket = new WebSocket("ws://81.107.155.88:3001/StreamChat");
+        this.serverSocket.onmessage = onMessage;
+    }
 
-serverSocket.onmessage = function(event) {
-   console.log(`Received message: ${event.data}`);
-};
+    send(content) {
+        this.serverSocket.send(JSON.stringify({
+            Sender: this.name,
+            Content: content
+        }));
+    }
+}
+
+connection = new UserConnection("User", function(event) {
+    console.log(event.data);
+});
