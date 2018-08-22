834dad3d32db8ee947dd5bd3ef6cf322ef111157
Aleksbgbg <kozichka01@abv.bg>
2018-08-22 11:06:39 +0100

    [StreamRegisteredEventArgs] Add class to store StreamId from events.

---
+++ Server/EventArgs/StreamRegisteredEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Twitch.Streams.Server.EventArgs
+{
+    using System;
+
+    internal class StreamRegisteredEventArgs : EventArgs
+    {
+        public StreamRegisteredEventArgs(string streamId)
+        {
+            StreamId = streamId;
+        }
+
+        public string StreamId { get; }
+    }
+}
