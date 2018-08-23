d56c15287d2f67d7c54f5434e163f26c75884c55
Aleksbgbg <kozichka01@abv.bg>
2018-08-23 18:36:49 +0100

    [ChatTransmission] Add model class for personal chat transmissions, which have an opcode, as well as a message.

---
+++ Server/Models/ChatTransmission.cs
@@ -0,0 +1,20 @@
+﻿namespace Twitch.Streams.Server.Models
+{
+    using Newtonsoft.Json;
+
+    internal class ChatTransmission
+    {
+        [JsonConstructor]
+        public ChatTransmission(OpCode opCode, Message message)
+        {
+            OpCode = opCode;
+            Message = message;
+        }
+
+        [JsonProperty("OpCode")]
+        public OpCode OpCode { get; }
+
+        [JsonProperty("Message")]
+        public Message Message { get; }
+    }
+}
