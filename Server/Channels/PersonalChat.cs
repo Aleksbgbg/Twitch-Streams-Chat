9d2bbce6478c34a31c8728227101ab4fe264238d
Aleksbgbg <kozichka01@abv.bg>
2018-08-23 18:30:33 +0100

    [PersonalChat] Add class, similar to StreamChat, for chats between users.

---
+++ Server/Channels/PersonalChat.cs
@@ -0,0 +1,32 @@
+﻿namespace Twitch.Streams.Server.Channels
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    using Twitch.Streams.Server.Models;
+
+    using WebSocketSharp;
+    using WebSocketSharp.Server;
+
+    internal class PersonalChat : WebSocketBehavior
+    {
+        protected override void OnOpen()
+        {
+            base.OnOpen();
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Sessions.Broadcast(e.Data);
+
+            Message receivedMessage = JsonConvert.DeserializeObject<Message>(e.Data);
+            Console.WriteLine("{0}: {1}", receivedMessage.Sender, receivedMessage.Content);
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            base.OnClose(e);
+        }
+    }
+}
